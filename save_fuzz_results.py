754d6dddaba753c08ee7bb1c2b42ce4c31803b14
YuChen <fishbabymail@gmail.com>
2024-11-18 21:34:52 -0800

    a common function to save fuzz results to file

---
+++ save_fuzz_results.py
@@ -0,0 +1,23 @@
+import os
+
+
+def save_results(results, fuzztype, base_path = None):
+    # Make results directory
+    if not base_path:
+        base_path = os.getcwd()
+    filedir = os.path.join(base_path, "fuzzing_results")
+    if not os.path.exists(filedir):
+        os.makedirs(filedir)
+    filename = fuzztype + "_" + "results" + ".txt"
+    filepath = os.path.join(filedir, filename)
+    if not os.path.isfile(filepath):
+        with open(filepath, 'w') as f:
+            pass
+    for query, resp in results.items():
+        # query = ast.literal_eval(query)
+        with open(filepath, 'a') as f:
+            f.write("------------------Payload:-------------------\n")
+            f.write(f"{query}\n")
+            f.write("------------------Response:-------------------\n")
+            f.write(f"{resp}\n")
+    return filepath
