d55b5903aed7d0b933fcb6de363b1aa160bd2af9
YuChen <fishbabymail@gmail.com>
2024-11-18 17:19:10 -0800

    identify valid queries from llama_queries

--- identify_valid_query.py
+++ identify_valid_query.py
@@ -16,7 +16,7 @@ def get_queries(filedir, filename):
         raise FileNotFoundError(filepath)
     with open(filepath, 'r') as f:
         queries = json.load(f)
-    query_list = queries['query']
+    query_list = queries["query"]
     return query_list
 
 
@@ -29,27 +29,43 @@ def request_query(url, query):
 
     try:
         response = requests.post(url, json=payload)
+        print(response.text)
     except:
-        return False
-    # print(response.text)
+        logger.error("Exception request for query: {}".format(query))
+        return
     if response.status_code == 200:
         response_text = response.text
         logger.info("Response:{}".format(response_text))
         logger.info("This query is valid.")
-        return True
+        return query
     else:
-        logger.info("Failed to fetch data: {}".format(response.status_code))
-        # logger.info("Error msg:", response.text)
-        return False
+        logger.error("Failed to fetch data: {}".format(response.status_code))
+        return
 
-def main(url, filedir, filename):
+
+def run(url):
+    valid_queries = []
+    filedir = os.path.join(os.getcwd(), "llama_query")
+    filename = "llama_queries.json"
     query_list = get_queries(filedir, filename)
     for i, query in enumerate(query_list):
         res = request_query(url, query)
+        if not res:
+            valid_queries.append(query)
+
+    res = {
+        "query": valid_queries
+    }
+
+    save_name = "valid_query.json"
+    filename = os.path.join(filedir, save_name)
+    with open(filename, 'w') as f:
+        json.dump(res, f)
+    logger.info("Saved valid query to {}".format(filename))
 
 
 if __name__ == '__main__':
     url = "https://rickandmortyapi.com/graphql"
-    main(url, "llama_query", "rick_query_ep.json")
+    run(url)
 
 
